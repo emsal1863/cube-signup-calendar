2eba3e015f9508b5f2b6ac32b92acc5a56a6b191
Em Sal <absolutezero2a03@gmail.com>
2016-12-01 16:45:23 -0800

    able to do event editing!

--- flaskcalendar/static/dialog.jsx
+++ flaskcalendar/static/dialog.jsx
@@ -1,11 +1,40 @@
 class Dialog extends React.Component {
 
 
-    constructor(x, y) {
+    constructor(x, y, calEvent, calendar) {
         super();
+
+        this.state = {person: ''}
+
         this.x = x;
         this.y = y;
+        this.calEvent = calEvent;
+        this.calendar = calendar;
 
+        this.state = {
+            personValue: ''
+        };
+
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+
+    }
+
+    handleSubmit(event) {
+        var calEventId = this.props.calEvent._id;
+
+        var clientEvents = this.props.calendar.fullCalendar('clientEvents');
+        var calEventTrue = clientEvents.find( (calEvent) => calEvent._id === calEventId);
+
+        calEventTrue.title = this.state.personValue;
+
+        this.props.calendar.fullCalendar('updateEvent', calEventTrue);
+        this.props.calendar.fullCalendar('renderEvent', calEventTrue);
+        event.preventDefault();
+    }
+
+    handleChange(event) {
+        this.setState({personValue: event.target.value});
     }
 
     render() {
@@ -14,6 +43,14 @@ class Dialog extends React.Component {
             clear: 'both'
         };
 
-        return <h1 style={divStyle}>aww yeah</h1>;
+        return (
+            <div style={divStyle}>
+                <h1>aww yeah</h1>
+                <form onSubmit = {this.handleSubmit}>
+                    <input name="person" onChange={this.handleChange} />
+                    <input type="submit" value="submit" />
+                </form>
+            </div>
+        );
     }
 }
