a54422f7312ecbb223c693e6d85da9b05b569b7f
Em Sal <absolutezero2a03@gmail.com>
2016-12-11 20:48:30 -0800

    some unit tests and documentation -- nice to get this working

--- db/db_connection.py
+++ db/db_connection.py
@@ -2,8 +2,12 @@ import os
 import psycopg2
 import urlparse
 
-
 def init():
+    """
+    Initialize the database connection.
+
+    The particular environment variable DATABASE_URL comes from the heroku specification for using postgres.
+    """
     urlparse.uses_netloc.append("postgres")
     url = urlparse.urlparse(os.environ["DATABASE_URL"])
 
@@ -14,12 +18,46 @@ def init():
         host=url.hostname,
         port=url.port
     )
+    return conn
 
 def insert_time(conn, start_time, end_time, person):
-    cur = conn.cursor()
-    cur.execute("INSERT INTO calendar_events (start_time, end_time, person) VALUES (to_timestamp(%s, 'YYYY-MM-DDTHH:MI'), to_timestamp(%s,'YYYY-MM-DDTHH:MI'), %s)", start_time.isoformat(), end_time.isoformat(), person)
-    conn.commit()
+    """
+    Insert a calendar event into the database.
 
-def close(conn, cur):
+    Takes in a psycopg2 connection, two datetime.datetime types for start and end time, and a person string.
+
+    Returns the id of the database record just inserted
+    """
+    cur = conn.cursor()
+    cur.execute("INSERT INTO calendar_events (start_time, end_time, person) VALUES (to_timestamp(%s, 'YYYY-MM-DDTHH:MI'), to_timestamp(%s,'YYYY-MM-DDTHH:MI'), %s) returning id", (start_time.isoformat(), end_time.isoformat(), person))
+    conn.commit()
+    tmp = cur.fetchone()
     cur.close()
+    return tmp
+
+def delete_time(conn, event_id):
+    """
+    Deletes the calendar event with the given `event_id` from the database.
+
+    Takes in a psycopg2 connection and an event_id as a number (resolves to long).
+
+    No return value. Raises a psycopg2 error if something goes wrong.
+    """
+    cur = conn.cursor()
+    cur.execute("DELETE FROM calendar_events WHERE id = %s", (event_id,))
+    conn.commit()
+    cur.close()
+
+def edit_time(conn, event_id, new_event_data):
+    """
+    Edit a time object in the database.
+
+    Not implemented yet.
+    """
+    pass
+
+def close(conn):
+    """
+    Closes the database connection.
+    """
     conn.close()
--- db/db_connection_test.py
+++ db/db_connection_test.py
@@ -4,14 +4,46 @@ import psycopg2
 import urlparse
 import os
 import mock
+import datetime
 from mock import patch
 
 class TestDBMethods(unittest.TestCase):
+    def setUp(self):
+        self.conn = psycopg2.connect(
+            database='postgres'
+        )
+        cur = self.conn.cursor()
+
+        drop_table_command = "DROP TABLE if exists dummy_calendar_events;"
+
+        create_table_command = """CREATE TABLE IF NOT EXISTS dummy_calendar_events (
+            id BIGSERIAL PRIMARY KEY,
+            start_time TIMESTAMP,
+            end_time TIMESTAMP,
+            person VARCHAR(512)
+        );"""
+
+        cur.execute(drop_table_command)
+        cur.execute(create_table_command)
+        self.conn.commit()
+        cur.close()
+
     @patch.dict(os.environ,{'DATABASE_URL': 'postgres'})
     @patch('psycopg2.connect')
-    def test_insert(self, psycopg2_mock):
-        #psycopg2_mock.return_value.cursor.
-        self.assertTrue(True)
+    def test_insert_and_delete(self, psycopg2_mock):
+        psycopg2_mock.return_value = self.conn
+        conn = db_connection.init()
+
+        # insert_result should be the id of the element just inserted
+        insert_result = db_connection.insert_time(conn, datetime.datetime.today(), datetime.datetime.today(), "asdf")[0]
+
+        # id shouldn't be 0
+        self.assertTrue(insert_result)
+
+        db_connection.delete_time(conn, insert_result)
+
+    def tearDown(self):
+        self.conn.close()
 
 if __name__ == "__main__":
-    unittest.main()
+    unittest.main(verbosity=2)
